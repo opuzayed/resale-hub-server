5483d7ca922f3194283861fb617f7a3496822734
opuzayed <zayedopu4933@gmail.com>
2022-12-28 23:52:07 +0600

    username and password set

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-//const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-//const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -12,10 +12,17 @@ app.use(express.json());
 
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rsulfhn.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
 app.get('/', (req, res) => {
     res.send('Resale server is running');
   });
 
-  app.listen(port, () => {
+app.listen(port, () => {
     console.log(`Resale server is running on  ${port}`);
   });
+  
